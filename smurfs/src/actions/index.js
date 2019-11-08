84ef83e008dc9a827eb0b91e051f360d5f84fb05
Mark Halls <mark.halls@gmail.com>
2019-11-08 11:08:22 -0700

    Add post action

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -24,3 +24,19 @@ export const fetchSmurfs = () => dispatch => {
     .then(res => dispatch(smurfsLoadSuccess(res.data)))
     .catch(err => dispatch(smurfsLoadFailed(err.data)));
 };
+
+export const postSmurf = payload => dispatch => {
+  dispatch(smurfsLoading());
+
+//   payloadShape: {
+//     name: "Sleepy",
+//     age: 200,
+//     height: "5cm",
+//     id: 1
+//   }
+
+  return axios
+    .post(`http://localhost:3333/smurfs`, payload)
+    .then(() => dispatch(fetchSmurfs()))
+    .catch(err => dispatch(smurfsLoadFailed(err.data)));
+};
